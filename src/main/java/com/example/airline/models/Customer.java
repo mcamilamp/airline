56e50914dad7b3cdd72d4ea8d4be4bafe730d086
Camilo Benavides <benavidescamilo199@gmail.com>
2024-09-05 02:14:24 -0500

    Create missing classes

--- src/main/java/com/example/airline/models/Customer.java
+++ src/main/java/com/example/airline/models/Customer.java
@@ -1,28 +1,35 @@
 package com.example.airline.models;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+import java.io.Serializable;
 
 @Entity
 @Table(name = "customer")
-public class Customer {
+public class Customer implements Serializable {
 
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idCustomer;
 
-    @Column(name = "NAME")
+    @Column(name = "name")
     private String name;
 
-    @Column(name = "LAST_NAME")
+    @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "ADDRESS")
+    @Column(name = "address")
     private String address;
 
-    @Column(name = "PHONE")
+    @Column(name = "phone")
     private String phone;
 
-    @Column(name = "EMAIL")
+    @Column(name = "email")
     private String email;
 
 }
