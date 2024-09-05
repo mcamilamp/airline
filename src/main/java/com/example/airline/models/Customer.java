14f0e9729bc0ad31c779b4a63b2983c7d1d3e00f
mcamilamp <camimerpa@gmail.com>
2024-09-04 21:20:02 -0500

    :,|

---
+++ src/main/java/com/example/airline/models/Customer.java
@@ -0,0 +1,28 @@
+package com.example.airline.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCustomer;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+}
